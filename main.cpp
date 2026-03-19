f1aab510936b42f5102098d2811cb9543ea143ce
kalcium <162475090+Maliik2000@users.noreply.github.com>
2026-03-19 14:54:07 -0500

    Add greeting for Malik

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     cout << "Hello, World from feature 2!" << endl;
         cout << "Hello, Chris!" << endl;
+    cout << "Malik is Awesome" << endl;
 
     return 0;
 }
